4e1c274f24b75e98bb4f1e9c0b63cde47847276c
Hariventan <hariventan_mohan@astro.com.my>
2023-02-06 14:37:23 +0800

    to do list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+
 
 function App() {
+const [todoItem, setTodoItem] = useState('')
+const [list, setList] = useState([])
+
+
+const todoUpdate = (e) =>{
+  setTodoItem(e.target.value)
+}
+
+const click = ()=> {
+  if (!todoItem ){
+    alert('Enter something')
+    return;
+    
+}
+
+// console.log(todoItem)
+const item = {
+  id : Math.floor(Math.random()*500),
+  value: todoItem,
+  done: false
+}
+  setList(oldList => [...oldList,item])
+  setTodoItem("")
+ 
+}
+
+
+
+const handleKeyDown = (e) => {
+  if (!todoItem ){
+    alert('Enter something')
+    return;
+    
+}
+
+  const item = {
+    id : Math.floor(Math.random()*500),
+    value: todoItem,
+    done: false
+  }
+
+  if (e.key === 'Enter') {
+    setList(oldList => [...oldList,item])
+    setTodoItem("")
+
+ 
+  }
+};
+
+const deleted = (index) => {
+    let newArray = list.filter((item, i) => i !== index)
+    setList(newArray)
+}
+
+const strike = (index) => {
+    setList(
+      list.map(( item, i) => {
+      if(i === index ){
+        // console.log(item.index)
+        return { value: item.value, done: !item.done };
+      }
+      // console.log(item1)
+      return item;
+     
+    }))
+
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+    <div className="heading">
+      
+         {/* Header */}
+
+        <h2>To-do List</h2>
+
+            {/* Input todo & button */}
+        <div className='Header'>
+          <input
+          onKeyDown={handleKeyDown}
+          value={todoItem} placeholder={'Write Something ...'} onChange={todoUpdate}/>
+          <button className='add' onClick={click}>Add</button>
+        </div>
+
+        
+        {/* List all Items */}
+
+
+
+        {list.map((item, index) =>{ 
+          return(
+            <div key={index} >
+              <span style={{ textDecoration: item.done ? "line-through" : "none" }}>{item.value}</span>  
+              <button className='strike' onClick={() =>strike(index)} >✅</button>
+              <button className='del' onClick={() =>deleted(index)} >❎</button>
+            
+           
+             </div>
+          ) })}
+      
+    </div>
     </div>
   );
 }
